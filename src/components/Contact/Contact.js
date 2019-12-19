d001752062ebd6a6b137605dfa92daf6ab26a5b1
Martin Studna <martin.studna2@gmail.com>
2019-12-20 00:23:47 +0100

    fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,23 @@
 import React from "react"
 import "./Contact.css"
+import { makeStyles } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
 
 const contact = props => (
   <div className="contact">
     <div className="contact-items">
       <div className="contact-title">Contact Me</div>
       <div className="contact-subtitle"></div>
+      <div>
+        <form noValidate autoComplete="off">
+          <TextField
+            id="standard-basic"
+            label="Name"
+            style={{ marginRight: "40px" }}
+          />
+          <TextField id="standard-basic" label="Email" />
+        </form>
+      </div>
     </div>
   </div>
 )
