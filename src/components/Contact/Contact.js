8cdbfd5705a464014d53fc9d5deb10c506b71677
Martin Studna <martin.studna2@gmail.com>
2019-12-21 15:40:49 +0100

    fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "./Contact.css"
-import { makeStyles } from "@material-ui/core/styles"
 import TextField from "@material-ui/core/TextField"
 
 const contact = props => (
