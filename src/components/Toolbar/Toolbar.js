983d784c32013b574e1242ea766fc4397d208781
Martin Studna <martin.studna2@gmail.com>
2019-12-22 13:28:29 +0100

    fix

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,30 +3,14 @@ import "./Toolbar.css"
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton"
 
 const toolbar = props => (
-  <header className="toolbar">
+  <header className="toolbar" id="toolbar">
     <nav className="toolbar_navigation">
-      <div className="toolbar_toggle-button">
-        <DrawerToggleButton click={props.drawerClickHandler} />
-      </div>
       <div className="toolbar_logo">
         <a href="/">Martin Studna</a>
       </div>
       <div className="spacer" />
-      <div className="toolbar_navigation_items">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/Workshops">Workshops</a>
-          </li>
-          <li>
-            <a href="/Blog">Blog</a>
-          </li>
-          <li>
-            <a href="/Contact">Contact</a>
-          </li>
-        </ul>
+      <div className="toolbar_toggle-button">
+        <DrawerToggleButton click={props.drawerClickHandler} />
       </div>
     </nav>
   </header>
--- src/screens/BlogScreen/BlogScreen.js
+++ src/screens/BlogScreen/BlogScreen.js
@@ -1,12 +1,7 @@
-import React, { Component } from "react";
-
-
+import React, { Component } from "react"
+import "./BlogScreen.css"
 export default class BlogScreen extends Component {
   render() {
-    return (
-      <div>
-
-      </div>
-    )
+    return <div className="blog"></div>
   }
 }
