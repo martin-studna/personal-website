4eeeb4549790525deb64974ac06d17a2011cfa03
Martin Studna <martin.studna2@gmail.com>
2019-12-22 11:04:01 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React, { Component } from "react";
-import Toolbar from "./components/Toolbar/Toolbar";
-import SideDrawer from "./components/SideDrawer/SideDrawer";
-import Backdrop from "./components/Backdrop/Backdrop";
-import AOS from 'aos';
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomeScreen from './screens/HomeScreen/HomeScreen'
-import WorkshopsScreen from "./screens/WorkshopsScreen/WorkshopsScreen";
-import BlogScreen from "./screens/BlogScreen/BlogScreen";
-import ContactScreen from "./screens/ContactScreen/ContactScreen";
-import ProjectScreen from "./screens/ProjectScreen/ProjectScreen";
-import 'aos/dist/aos.css'
+import React, { Component } from "react"
+import Toolbar from "./components/Toolbar/Toolbar"
+import SideDrawer from "./components/SideDrawer/SideDrawer"
+import Backdrop from "./components/Backdrop/Backdrop"
+import AOS from "aos"
+import "./App.css"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen/HomeScreen"
+import WorkshopsScreen from "./screens/WorkshopsScreen/WorkshopsScreen"
+import BlogScreen from "./screens/BlogScreen/BlogScreen"
+import ContactScreen from "./screens/ContactScreen/ContactScreen"
+import ProjectScreen from "./screens/ProjectScreen/ProjectScreen"
+import "aos/dist/aos.css"
 
 class App extends Component {
   state = {
     SideDrawerOpen: false
-  };
+  }
 
   componentDidMount() {
-    AOS.init({once: true, duration: 1200})
+    AOS.init({ once: true, duration: 1000 })
   }
 
   drawerToggleClickHandler = () => {
     this.setState(prevState => {
-      return { SideDrawerOpen: !prevState.SideDrawerOpen };
-    });
-  };
+      return { SideDrawerOpen: !prevState.SideDrawerOpen }
+    })
+  }
 
   backdropClickHandler = () => {
-    this.setState({ SideDrawerOpen: false });
-  };
+    this.setState({ SideDrawerOpen: false })
+  }
 
   render() {
-    let backDrop;
+    let backDrop
 
     if (this.state.SideDrawerOpen) {
-      backDrop = <Backdrop click={this.backdropClickHandler} />;
+      backDrop = <Backdrop click={this.backdropClickHandler} />
     }
 
     return (
@@ -45,14 +45,14 @@ class App extends Component {
         {backDrop}
         <Switch>
           <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/Workshops" component={WorkshopsScreen}/>
-          <Route exact path="/Blog" component={BlogScreen}/>
-          <Route exact path="/Contacts" component={ContactScreen}/>
-          <Route exact path="/Projects" component={ProjectScreen}/>
+          <Route exact path="/Workshops" component={WorkshopsScreen} />
+          <Route exact path="/Blog" component={BlogScreen} />
+          <Route exact path="/Contacts" component={ContactScreen} />
+          <Route exact path="/Projects" component={ProjectScreen} />
         </Switch>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
