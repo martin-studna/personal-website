36bda589ca1aadb921f3f2da602e7a4990e58a6c
Martin Studna <martin.studna2@gmail.com>
2020-01-22 14:11:33 +0100

    fix

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -4,28 +4,33 @@ import "./NavigationBar.css";
 const NavigationBar = props => {
 
   const [nav, setNav] = useState('nav-links')
+  const [fade, setFade] = useState('')
 
   return (
     <nav>
-      <div className="hamburger" onClick={() => {
+    Martin Studna
+      <div className="hamburger"  onClick={() => {
           if (nav === 'nav-links') {
             setNav('nav-links open')
+            setFade('fade')
           }
-          else
-            setNav('nav-links')
+          else{
+            setFade('', setNav('nav-links'))
+          }
+            
       }}>
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
       <ul className={nav}>
-        <li>
+        <li className={fade}>
           <a href="/">Home</a>
         </li>
-        <li>
+        <li className={fade}>
           <a href="/About">About</a>
         </li>
-        <li>
+        <li className={fade}>
           <a href="/Contact">Contact</a>
         </li>
       </ul>
