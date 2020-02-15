4298094d306d35ad1ecda3ff1adf2124144df873
Martin Studna <martin.studna2@gmail.com>
2020-02-15 19:53:10 +0100

    skillbars

--- src/components/Skillbar/Skillbar.js
+++ src/components/Skillbar/Skillbar.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./Skillbar.css";
 
 export default class Skillbar extends React.Component {
-
   state = {
     style: "skill_inner-bar"
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener("scroll", () =>{this.animate(document.getElementById(this.props.id))})
+    window.addEventListener("scroll", () => {
+      this.animate(document.getElementById(this.props.id));
+    });
   }
 
   inView(element) {
@@ -17,31 +18,29 @@ export default class Skillbar extends React.Component {
     var elementHeight = element.clientHeight;
     // get number of pixels that the document is scrolled
     var scrollY = window.scrollY || window.pageYOffset;
-    
+
     // get current scroll position (distance from the top of the page to the bottom of the current viewport)
     var scrollPosition = scrollY + windowHeight;
     // get element position (distance from the top of the page to the bottom of the element)
-    var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
-    
+    var elementPosition =
+      element.getBoundingClientRect().top + scrollY + elementHeight;
+
     // is scroll position greater than element position? (is element in view?)
     if (scrollPosition > elementPosition) {
       return true;
     }
-    
+
     return false;
   }
 
   animate(element) {
-    if (element === null || typeof element === "undefined")
-      return
-    console.log("is not in viewport");
+    if (element === null || typeof element === "undefined") return;
+
     // is element in view?
-    if (this.inView(element)) {
-        console.log("is in viewport");
-        
-        // element is in view, add class to element
-        this.setState({style: "skill_inner-bar skill_inner-bar-20"})
-        //element.classList.add('skill_inner_bar-20');
+    if (this.inView(element) && this.state.style !== "skill_inner-bar skill_inner-bar-val") {
+      // element is in view, add class to element
+      this.setState({ style: "skill_inner-bar skill_inner-bar-val" });
+      //element.classList.add('skill_inner_bar-20');
     }
   }
 
@@ -50,9 +49,16 @@ export default class Skillbar extends React.Component {
       <div className="skill" data-aos="fade-zoom in">
         <h3 className="skill_type">{this.props.skill}</h3>
         <div className="skill_outer-bar">
-          <div id={this.props.id} className={this.state.style}></div>
+          <div
+            id={this.props.id}
+            style={{
+              width: this.props.percent,
+              background: this.props.color
+            }}
+            className={this.state.style}
+          ></div>
         </div>
       </div>
-    )  
+    );
   }
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,10 +9,21 @@ const skills = props => (
         Skills
       </div>
       <div className="skills-progress-bars">
-        <Skillbar id="first" skill="C#"/>
-        <Skillbar id="second" skill="Java"/>
-        <Skillbar id="third" skill="Javascript"/>
-        <Skillbar id="fourth" skill="C++"/>
+        <Skillbar id="1" skill="C#" percent="70%" color="#651f77" />
+        <Skillbar id="2" skill="Java" percent="50%" color="ee2d38" />
+        <Skillbar id="3" skill="Javascript" percent="60%" color="#eed818" />
+        <Skillbar id="4" skill="C/C++" percent="45%" color="#6296cc" />
+        <Skillbar id="5" skill="Kotlin" percent="60%" color="#7472E3" />
+         <Skillbar id="6" skill="Python" percent="40%" color="#0ac71a" />
+        {/*<Skillbar id="7" skill="Docker" percent="70%" color="green" />
+        <Skillbar id="8" skill="Android" percent="70%" color="green" />
+        <Skillbar id="9" skill="React" percent="70%" color="green" />
+        <Skillbar id="10" skill="React Native" percent="70%" color="green" />
+        <Skillbar id="11" skill="Unix Administration" percent="70%" color="green" />
+        <Skillbar id="12" skill="NodeJs" percent="70%" color="green" />
+        <Skillbar id="13" skill="ASP .NET" percent="70%" color="green" />
+        <Skillbar id="14" skill="NodeJs" percent="70%" color="green" />
+        <Skillbar id="15" skill="OpenCV" percent="70%" color="green" /> */}
       </div>
     </div>
   </div>
