d79c90135ec865b345c92226c060ae9441d0c78a
Martin Studna <martin.studna2@gmail.com>
2020-02-22 13:41:42 +0100

    Releasing the website

---
+++ src/lang.js
@@ -0,0 +1,87 @@
+var content = {
+  subtitle: { en: "Computer scientist", cs: "Softwarový vývojář" },
+  about: { en: "About", cs: "O mně" },
+  "about-p1": {
+    en:
+      "Hello, my name is Martin Studna. I am a freelance computer scientist and contractor interested in Computer Vision and Artificial Intelligence. I am currently finishing my bachelor degree in Computer Science from Faculty of Mathematics and Physics, Charles University in Prague.",
+    cs:
+      "Zdravím Vás, moje jméno je Martin Studna. Jsem softwárový vývojář a kontraktor zajímající se o Počítačové vidění a  Umělou inteligenci. Momentálně dokončuji bakalářské studium z Informatiky na Matematicko-fyzikální fakultě Univerzity Karlovy"
+  },
+  "about-p2": {
+    en:
+      "Moreover, I have a part-time job in IBM where I focus on chatbot development. On top of that, I have smaller contracts which include development of mobile applications.",
+    cs:
+      "Ke škole mám ještě pracovní úvazek ve společnosti IBM, kde jsem v týmu, který se zaměřuje na vývoj a implementaci chatbotů. K tomu se věnuji menším projektům z oblasti mobilního vývoje a školením různých technologií."
+  },
+  "ibm-title": { en: "Software developer", cs: "Softwarový vývojář" },
+  "ibm-place": {
+    en: "Prague, The Capital, Czech Republic",
+    cs: "Praha, Česká republika"
+  },
+  "ibm-description": {
+    en:
+      "Development and integration of technologies like IBM Watson or IBM Cloud functions",
+    cs: "Vývoj a integrace technologiích spjatých s IBM Watson a IBM Cloud"
+  },
+
+  "acresia-title": {
+    en: "Mobile Application Developer",
+    cs: "Mobilní vývojář"
+  },
+  "acresia-place": {
+    en: "Prague, The Capital, Czech Republic",
+    cs: "Praha, Česká republika"
+  },
+  "acresia-description": {
+    en:
+      'As a part of the contract with the company Acresia consulting I was developing mobile application "Zíráním nepomůžeš" for platforms Android and IOS. Firstly, I created a smaller application which was written in React Native. Then, I created the application natively.',
+    cs:
+      'V rámci smlouvy se společnosti Acresia consulting jsem se dostal k vývoji mobilní aplikace "Zíráním nepomůžeš" jak na platformu Android tak IOS. Vyvíjel jsem nejdříve menší verzi aplikace, která byla napsána pomocí frameworku React native. Později jsem vytvořil aplikaci nativně.'
+  },
+
+  "qest-title": { en: ".NET Developer", cs: ".NET vývojář" },
+  "qest-place": {
+    en: "Prague, The Capital, Czech Republic",
+    cs: "Praha, Česká republika"
+  },
+  "qest-description": {
+    en:
+      "Project Haidy - intelligent buildings. Development of cloud application, which communicates with house technologies. I am part of C# team, which is developing backend of the application. We are also using technologies like Docker or MongoDB.",
+    cs:
+      "Projekt Haidy - inteligentní budovy. Vývoj cloudové aplikace, která komunikuje s domácími technologiemi. Jako .NET vývojář jsem implementoval některé backendové funkce projektu Haidy - REST API, MongoDB, Azure, ..."
+  },
+
+  "coingroup-title": { en: ".NET Developer", cs: ".NET vývojář" },
+  "coingroup-place": {
+    en: "Prague, The Capital, Czech Republic",
+    cs: "Praha, Česká republika"
+  },
+  "coingroup-description": {
+    en:
+      "Development of exchange website with virtual currencies. Technology of the website was based on ASP.NET",
+    cs: "Vývoj směnárny s virtuálními měnami pomocí ASP.NET"
+  },
+
+  skills: { en: "Skills", cs: "Dovednosti" },
+  contact: { en: "Contact Me", cs: "Kontaktujte mě" },
+  name: { en: "Name", cs: "Jméno" },
+  subject: { en: "Subject", cs: "Předmět" },
+  message: { en: "Message", cs: "Zpráva" },
+  send: { en: "Send", cs: "Poslat" },
+
+  home: { en: "Home", cs: "Domů" },
+  projects: { en: "Projects", cs: "Projekty" },
+  workshops: { en: "Workshops", cs: "Workshopy" },
+  contacts: { en: "Contacts", cs: "Kontakt" },
+
+  "workshops-description-p1": {
+    en:
+      "I have currently started cooperation with Ondřej Sika. I organize with him workshops on several topics from Software development. We can show you how to make programming faster with nowadays tools like Docker, Git, Kubernetes and etc.",
+    cs: "Momentálně spolupracuji s Ondřejem Sikou na technických školeních zejména v oblasti DevOps. Jedná se o jednodenní nebo vicedenní školení zaměřené na technologie jako jsou Docker, Kubernetes nebo Git. Na školeních si ukážeme praktické příklady těchto technologií, které se mohou hodit buď při vývoji nebo administraci serveru."
+  },
+  "workshops-description-p2": {
+    en:
+      "Futhermore, I am also higly experienced with programming languages/frameworks like React Native, NodeJs, Java, .NET and many other technologies.",
+    cs: "Mímo výše zmíněné technologie jsem ještě zkušený vývojář v oblastech webového nebo mobilního vývoje, kde mohu v těchto oblastech nabídnout školení případně konzultaci."
+  }
+};
