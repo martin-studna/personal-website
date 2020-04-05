aa3ed51fb28c1fead6b58b353a54821c26ad6927
Martin Studna <martin.studna2@gmail.com>
2020-04-05 19:39:08 +0200

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,42 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+require('dotenv')
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.email,
+    pass: process.env.password
+  }
+})
 
 const app = express()
-const port = process.env.port
+const port = process.env.port || 3002
 
-app.get('/send', () => {
+app.use(express.json({ limit: '1mb'}))
+app.use(
+  cors({
+    origin: process.env.allow_origin || '*'
+  })
+)
 
+app.post('/send', (res) => {
+  console.log(res.body);
+
+  const mailOptions = {
+    from: res.body.email,
+    to: process.env.email,
+    subject: res.body.subject,
+    html: res.body.message
+  }
+  
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error)
+      console.log(error)
+    else
+      console.log(info);
+  })
 })
 
 
